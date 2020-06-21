a8ceb2d505c4d0ed0a9e6e9d95b859d0dfcf7435
gazorpazorp85 <paolo.groppi@gmail.com>
2020-06-21 17:17:28 +0300

    Updates

---
+++ precache-manifest.c1ab650d6394e0a44ee77565288643b8.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "a05a111be3711a5d3a5c6d2ad61a07e7",
+    "url": "/gazorpaweather-ca-page/index.html"
+  },
+  {
+    "revision": "08388c8dbc813f6362f4",
+    "url": "/gazorpaweather-ca-page/static/css/main.dcda2d93.chunk.css"
+  },
+  {
+    "revision": "b07027c12617f7f66274",
+    "url": "/gazorpaweather-ca-page/static/js/2.f31cb5a1.chunk.js"
+  },
+  {
+    "revision": "0749163b59fbee32225059cb60c18af6",
+    "url": "/gazorpaweather-ca-page/static/js/2.f31cb5a1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "08388c8dbc813f6362f4",
+    "url": "/gazorpaweather-ca-page/static/js/main.20440179.chunk.js"
+  },
+  {
+    "revision": "a0e24f1b1ef6a989bd0c",
+    "url": "/gazorpaweather-ca-page/static/js/runtime-main.7a49c1d5.js"
+  },
+  {
+    "revision": "39ac499c2a7856b24ffe3ffde4c8cc61",
+    "url": "/gazorpaweather-ca-page/static/media/15.39ac499c.png"
+  },
+  {
+    "revision": "0cf9cad1347244da8a9e197998e0eff9",
+    "url": "/gazorpaweather-ca-page/static/media/16.0cf9cad1.png"
+  },
+  {
+    "revision": "3c85da3e400a073c51b3ea73bdf5a130",
+    "url": "/gazorpaweather-ca-page/static/media/17.3c85da3e.png"
+  },
+  {
+    "revision": "421c13f1b1eb0ae947bb64472fec6baa",
+    "url": "/gazorpaweather-ca-page/static/media/41.421c13f1.png"
+  },
+  {
+    "revision": "85d339d916479f729938d2911b85bf1f",
+    "url": "/gazorpaweather-ca-page/static/media/Lato-Bold.85d339d9.ttf"
+  },
+  {
+    "revision": "2d36b1a925432bae7f3c53a340868c6e",
+    "url": "/gazorpaweather-ca-page/static/media/Lato-Regular.2d36b1a9.ttf"
+  },
+  {
+    "revision": "c46b805c0b91d20d2a785f606088f01a",
+    "url": "/gazorpaweather-ca-page/static/media/logo-white.c46b805c.png"
+  },
+  {
+    "revision": "60c916d57296dc8c3b964c9e247c5fd8",
+    "url": "/gazorpaweather-ca-page/static/media/paolo_groppi.60c916d5.jpg"
+  }
+]);
